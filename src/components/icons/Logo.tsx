af4925f0a685ccc0ddf345eac48e590ecb956678
Newton Botuem <escalarminhasvendas@gmail.com>
2025-07-02 17:53:09 +0000

    Algo deu errado, não houve alteração no tamanho do logotipo

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image';
 
 export default function Logo({ className }: { className?: string }) {
   return (
-    <div className={`flex items-center ${className}`}>
+    <div className={`flex items-center w-20 ${className}`}>
       <Image
         src="https://firebasestorage.googleapis.com/v0/b/cb-vempreender.firebasestorage.app/o/Imagens%20LP%2Flogo_vempreender.png?alt=media&token=b13f9567-e302-48ca-973a-d63a19daa17d"
         alt="Vempreender AI Logo"
         width={120}
         height={24}
-        className="object-contain"
+        className="h-auto w-full object-contain"
         priority
       />
     </div>
