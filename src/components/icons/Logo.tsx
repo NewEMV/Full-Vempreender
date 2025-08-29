51cd5040f45273ec5b761b50b06ff912a18990ed
Newton Botuem <escalarminhasvendas@gmail.com>
2025-08-29 21:47:32 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -4,7 +4,7 @@ export default function Logo({ className }: { className?: string }) {
   return (
     <div className={`flex items-center w-20 ${className}`}>
       <Image
-        src="https://firebasestorage.googleapis.com/v0/b/cb-vempreender.firebasestorage.app/o/Imagens%20LP%2Flogo_vempreender.png?alt=media&token=b13f9567-e302-48ca-973a-d63a19daa17d"
+        src="https://firebasestorage.googleapis.com/v0/b/cb-vempreender.firebasestorage.app/o/Imagens%20LP%2Flogo_vempreender.webp?alt=media&token=a98bbda2-4c3a-4933-8e90-873de1a44422"
         alt="Vempreender AI Logo"
         width={120}
         height={24}
