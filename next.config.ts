ff3c7f2850a467ca008c8ba2b786927f9dce8a30
Newton Botuem <escalarminhasvendas@gmail.com>
2025-08-30 21:51:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // force cache invalidation
   typescript: {
     ignoreBuildErrors: true,
   },
